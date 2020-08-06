eb745470c7de3d73928191fc86c6d47022e29b40
s1270187 <s1270187@u-aizu.ac.jp>
2020-08-06 22:46:44 +0900

    Hot:Initial

---
+++ Heads_or_Tails.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Heads_or_Tails{
+  public static void main(String[] args){
+    int ROUND = 3;
+    int NumberOfHeads = 0;
+    int NumberOfTails = 0;
+
+    Random random = new Random();
+
+
+    
+
+    
+
+    System.out.println("Tossing a coin");
+
+    for (int i = 1;i<=ROUND;i++){
+      if(random.nextInt(2)>0){
+        NumberOfHeads++;
+        System.out.println("Round " + i + ": Heads");
+      }
+      else{
+        NumberOfTails++;
+        System.out.println("Round " + i + ": Tails")
+      }
+    }
+    System.out.println("Heads: " + NumberOfHeads + ", Tails: " + NumberOfTails);
+
+    i
+  }
+}
